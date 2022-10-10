eb60cc3d1f626fdf055cc8462feb92f4bc33edef
ChunbaoYang <chunbaoapp@gmail.com>
2022-10-10 16:54:45 +0200

    new class name

--- html/html/js/oefeningen.js
+++ html/html/js/oefeningen.js
@@ -166,7 +166,7 @@ function reportMe() {
 	var yourmessage = $("textarea[name='your-message']");
 	yourmessage.val(game.email);
 	//
-	var yoursubmit = $("input[class='wpcf7-form-control wpcf7-submit']");
+	var yoursubmit = $("input[class='wpcf7-form-control has-spinner wpcf7-submit']");
 	yoursubmit.click();
 
 	showMessage("Kies een nieuwe oefening.");
